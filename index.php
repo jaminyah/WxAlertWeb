a21cef813ca70dcb0f9cb2989a2a63bb63149067
jaminyah <jaminyah@gmail.com>
2019-08-29 05:40:48 -0500

    Initial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WxAlert Support</title>
+    </head>
+    <body>
+        <header>
+                <img src="images/icons/app_icon64.jpg" alt="wxalert app icon">
+                <h1>Welcome to Support</h1>
+                <hr>
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item"><a href="/">Home</a></li>
+                        <li class="nav-item"><a href="/ui_flow.html">UI Flow</a></li>
+                        <li class="nav-item"><a href="/wx_icons.html">WX Icons</a></li>
+                        <li class="nav-item"><a href="/alerts.html">Alerts</a></li>
+                        <li class="nav-item"><a href="/diamond.html">Diamond Alerts</a></li>
+                        <li class="nav-item"><a href="/contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+        </header>
+        <article>
+            <h1></h1>
+        </article>
+
+         
+        <footer>
+            &copy; <?php echo date("Y"); ?> WxAlert
+        </footer>
+    </body>
+</html>            
